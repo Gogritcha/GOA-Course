0f8e1da09ca14e2d02dc359b321b7395dfd747ac
Gogritcha <nikagogritchiani9@gmail.com>
2024-02-16 13:32:41 +0400

    commit

---
+++ Day 4/Info.py
@@ -0,0 +1,8 @@
+name = "ნიკa"
+surname = "გოგრიჭიანი"
+age = 16
+#in 10 years
+print (f"ჩემი სახელია - {name + " " + surname}")
+print (f"მე ვარ {age} წლის")
+age += 10
+print (f"ჩემი ასაკი 10 წელში - {age}")
