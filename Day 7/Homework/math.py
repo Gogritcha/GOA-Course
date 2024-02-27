33907f5cbf9227b6463563ba20e2288160f754d6
Gogritcha <nikagogritchiani9@gmail.com>
2024-02-27 13:47:31 +0400

    commit

---
+++ Day 7/Homework/math.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+
+
+num3 = float(input("Enter 3rd number(decimal number): "))
+
+print(num1 + num3)
+print(num2 + num3)
+
+num4 = str(input("Enter 4th number"))
+
+print(num4 + str(num1))
+print(num4 + str(num2))
+print(num4 + str(num3))
+print(num4 + num4)
