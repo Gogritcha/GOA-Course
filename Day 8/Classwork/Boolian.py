7c5755d1341362c0876dae7aa82575a60a4c7071
Gogritcha <nikagogritchiani9@gmail.com>
2024-03-01 19:58:32 +0400

    commit

---
+++ Day 8/Classwork/Boolian.py
@@ -0,0 +1,8 @@
+Target_A = 40
+Target_B = 80
+
+Your_A = int(input('how many A did you do '))
+Your_B = int(input('how many B did you do '))
+
+print(Your_A >= Target_A or Your_B >= Target_B)
+print(Your_A >= Target_A and Your_B >= Target_B)
---
+++ Day 8/Homework/Varjishi.py
@@ -0,0 +1,8 @@
+target_Sit_up = 40
+target_Push_up = 80
+
+your_sit_ups = int(input('how many sit ups did you do '))
+your_Push_ups = int(input('how many push ups did you do '))
+
+print(your_sit_ups >= target_Sit_up or your_Push_ups >= target_Push_up)
+print(your_sit_ups >= target_Sit_up and your_Push_ups >= target_Push_up)
