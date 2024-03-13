911a92dc46c112ef13b435da71b9c74f9edd6247
Gogritcha <nikagogritchiani9@gmail.com>
2024-03-13 14:07:25 +0400

    commit

--- Day 11/Homework/if & while hw.py
+++ Day 11/Homework/if & while hw.py
@@ -13,11 +13,13 @@ if num1 % 2 == 0:
     print("your number is even")
 
 
-user_num = int(input("Enter your number: "))
-updated_num = user_num + 20
-while user_num < updated_num:
-    print(user_num)
-    user_num += 1
+sum=0
+for i in range(50, 101):
+    if i%2==1:
+        print(i)
+    if i>75:
+        sum = sum + i
+print(sum)
 
 
 user_num = int(input("Enter your number: "))
