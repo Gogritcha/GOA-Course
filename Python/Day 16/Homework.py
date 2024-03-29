20ce923faa6062939b3a39069cdaa0af464c92ff
Gogritcha <nikagogritchiani9@gmail.com>
2024-03-29 13:34:41 +0400

    commit

---
+++ Python/Day 16/Homework.py
@@ -0,0 +1,19 @@
+films = ["Ready player one", "Dune", "Free guy", "Shazam!"]
+print(films)
+print(films[0])
+
+
+cars = ["BMW", "Mercedes", "Ferrari", "Ford", "Pegani"]
+print(films,cars)
+
+
+name = "Nika"
+print(name[0],name[3])
+
+
+films[2] = "Cars"
+print(films[2])
+
+
+nums = [1,102,456,23,45,87,90,12,34,56]
+print(nums[0] + nums[9])
